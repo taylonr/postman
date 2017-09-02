504bd8f78e1e3f6bc38fbae38a5ea8dc5bad16e1
Nate Taylor <taylonr@gmail.com>
2017-09-02 15:12:00 -0500

    Adding auth to the create, delete and update routes.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,17 @@
+const basicAuth = require('express-basic-auth');
 const booksController = require('../books/book.controller');
 
+const authorize = basicAuth({
+  users: {'admin': 'admin'}
+});
+
 module.exports = (app) => {
-  app.delete('/books/:id', booksController.deleteById);
+  app.delete('/books/:id', authorize, booksController.deleteById);
   app.get('/books', booksController.list);
   app.get('/books/:id', booksController.getById);
-  app.post('/books', booksController.create);
-  app.put('/books/:id', booksController.updateById);
+
+  app.post('/books', authorize, booksController.create);
+  app.put('/books/:id', authorize, booksController.updateById);
 
   app.use((req, res) => {
     res.status(404).end();
