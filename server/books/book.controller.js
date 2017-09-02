b60e54383342440268e3b282f156cfb42d014b7a
Nate Taylor <taylonr@gmail.com>
2017-09-02 12:03:00 -0500

    Cleaning up mocking out responses.

--- server/books/book.controller.js
+++ server/books/book.controller.js
@@ -1,10 +1,13 @@
-const responses = require('../responses'),
-  ok = responses.ok;
+const responses = require('../responses');
 
 const Book = require('../models').book;
 
 module.exports = {
   list: (req, res) => {
-    return Book.all().then(ok(res));
+    return Book.all()
+      .then(responses.ok(res))
+      .catch((err) => {
+        responses.serverError(res, err);
+      });
   }
 };
--- server/books/book.controller.spec.js
+++ server/books/book.controller.spec.js
@@ -1,10 +1,13 @@
-const expect = require('chai').expect;
 const httpMocks = require('node-mocks-http');
 const td = require('testdouble');
+const responses = require('../responses');
 const Book = require('../models').book;
 const controller = require('./book.controller');
 
 describe('Books controller', () => {
+  afterEach(() => {
+    td.reset();
+  });
   describe('When getting a list of books', () => {
     it('Should return ok', () => {
       const req = httpMocks.createRequest();
@@ -13,8 +16,26 @@ describe('Books controller', () => {
       const all = td.replace(Book, 'all');
       td.when(all()).thenResolve({});
 
-      controller.list(req, res).then(() => {
-        return expect(res.statusCode).to.eql(200);
+      const ok = td.replace(responses, 'ok');
+
+      return controller.list(req, res).then(() => {
+        td.verify(ok(res));
+      });
+    });
+
+    describe('And the call fails', () => {
+      it('Should call server error', () => {
+        const all = td.replace(Book, 'all');
+        td.when(all()).thenReject('err');
+
+        const err = td.replace(responses, 'serverError');
+
+        const req = httpMocks.createRequest();
+        const res = httpMocks.createResponse();
+
+        return controller.list(req, res).then(() => {
+          td.verify(err(res, 'err'));
+        });
       });
     });
   });
