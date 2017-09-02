5c747ffc8b60019fa00f72e1c92bccbc37b38d99
Nate Taylor <taylonr@gmail.com>
2017-09-02 17:41:42 -0500

    Adding environment info to the seeder

---
+++ server/seeders/data/books.js
@@ -0,0 +1,24 @@
+module.exports = () => {
+  return [{
+    title: 'Don\'t Waste Your Life',
+    author: 'John Piper',
+    publicationDate: new Date(2003, 4, 16),
+    isbn: '1593281056',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },{
+    title: 'The Total Money Makeover',
+    author: 'Dave Ramsey',
+    publicationDate: new Date(2007, 1, 6),
+    isbn: '0785289089',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },{
+    title: 'Van Til\'s Apologetic',
+    author: 'Greg Bahnsen',
+    publicationDate: new Date(1998, 6, 1),
+    isbn: '0875520987',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }];
+};
---
+++ server/seeders/data/books.test.js
@@ -0,0 +1,10 @@
+module.exports = () => {
+  return [{
+    title: 'Don\'t Waste Your Life',
+    author: 'John Piper',
+    publicationDate: new Date(2003, 4, 16),
+    isbn: '1593281056',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }];
+};
