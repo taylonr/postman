0595bd25a60e5547af7e00ac72ab06a033e60b3a
Nate Taylor <taylonr@gmail.com>
2017-10-21 10:02:36 -0500

    Updating shell scripts

--- dev.sh
+++ dev.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 ENV=dev DB_PORT=5434 WEB_PORT=3000 docker-compose -p dev up -d
+echo "****************************"
+echo "* API Running on port 3000 *"
+echo "****************************"
--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 ENV=test DB_PORT=5433 WEB_PORT=3030 docker-compose -p test up -d
+echo "****************************"
+echo "* API Running on port 3030 *"
+echo "****************************"
