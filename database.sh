4219c7ca481a3fdda0d6a816e47e98d3d16ee5a5
Nate Taylor <taylonr@gmail.com>
2017-10-22 11:59:03 -0500

    Adding datafiles.

--- database.sh
+++ database.sh
@@ -1,4 +1,10 @@
+echo "Stopping web server"
+docker stop dev_web_1
+echo "Dropping & Recreating DB"
 docker exec -it $(docker ps | grep dev_db | awk '{ print $1 }') /bin/sh -c 'dropdb -U postgres postman_dev'
 docker exec -it $(docker ps | grep dev_db | awk '{ print $1 }') /bin/sh -c 'createdb -U postgres postman_dev'
+echo "Starting web server"
+docker start dev_web_1
+echo "Seeding database"
 docker exec -it $(docker ps | grep dev_web | awk '{ print $1 }') /bin/sh -c 'npm run db:migrate'
 docker exec -it $(docker ps | grep dev_web | awk '{ print $1 }') /bin/sh -c 'npm run db:seed'
--- database.test.sh
+++ database.test.sh
@@ -1,4 +1,10 @@
+echo "Stopping web server"
+docker stop test_web_1
+echo "Dropping & Recreating DB"
 docker exec -it $(docker ps | grep test_db | awk '{ print $1 }') /bin/sh -c 'dropdb -U postgres postman_test'
 docker exec -it $(docker ps | grep test_db | awk '{ print $1 }') /bin/sh -c 'createdb -U postgres postman_test'
+echo "Starting web server"
+docker start test_web_1
+echo "Seeding database"
 docker exec -it $(docker ps | grep test_web | awk '{ print $1 }') /bin/sh -c 'NODE_ENV=test npm run db:migrate'
 docker exec -it $(docker ps | grep test_web | awk '{ print $1 }') /bin/sh -c 'NODE_ENV=test npm run db:seed'
