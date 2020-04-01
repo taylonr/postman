9c3186934d810288eea0fb7ac02165152d0696da
Nate Taylor <taylonr@gmail.com>
2020-04-01 08:49:42 -0500

    Return 500 error when books don't have title & implement /search for books

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const jsonServer = require("json-server");
+const url = require("url");
 const env = process.env.NODE_ENV || "development";
+const { flatten } = require("ramda");
 
 const server = jsonServer.create();
-const router = env === "test" ? jsonServer.router("db.test.json") : jsonServer.router("db.json");
+const router =
+  env === "test"
+    ? jsonServer.router("db.test.json")
+    : jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
 
@@ -49,6 +54,139 @@ server.use((req, res, next) => {
   } else {
     next();
   }
+});
+
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    if(req.path.match(/books/) && !req.body.title){
+      res.status(500).send({error: "Title cannot be null"});
+      return;
+    }
+    req.body.createdAt = new Date().toISOString();
+    req.body.updatedAt = new Date().toISOString();
+    next();
+  } else if (req.method === "PUT") {
+    req.body.updatedAt = new Date().toISOString();
+    next();
+  } else {
+    next();
+  }
+});
+
+function fullUrl(req) {
+  return url.format({
+    protocol: req.protocol,
+    host: req.get("host"),
+    pathname: req.originalUrl
+  });
+}
+
+router.render = (req, res) => {
+  if (req.method === "DELETE") {
+    res.status(204).send();
+  }
+  if (req.method === "POST" && res.status === 200) {
+    res.locals.data.links = [
+      {
+        rel: "self",
+        href: `${fullUrl(req)}/${res.locals.data.id}`
+      }
+    ];
+  }
+  res.jsonp(res.locals.data);
+};
+
+server.post("/users", (req, res, next) => {
+  const db = router.db;
+
+  const collection = db.defaults({ wishlists: [] }).get("wishlists");
+
+  const newWishlist = collection
+    .insert({ name: `${req.body.firstName}'s List`, books: [] })
+    .write();
+
+  req.body.wishlistId = newWishlist.id;
+
+  next();
+});
+
+server.post("/wishlists/:wishlistId/books/:bookId", (req, res) => {
+  const db = router.db;
+
+  const wishlist = db
+    .defaults({ wishlists: [] })
+    .get("wishlists")
+    .find({ id: parseInt(req.params.wishlistId, 10) })
+    .value();
+
+  wishlist.books = wishlist.books || [];
+  const bookId = parseInt(req.params.bookId, 10);
+
+  const indexOf = wishlist.books.indexOf(bookId);
+
+  if (indexOf < 0) {
+    wishlist.books.push(bookId);
+  }
+
+  db.write();
+
+  res.sendStatus(204);
+});
+
+const getBooksForWishlist = (wishlistId, db) => {
+  const wishlist = db
+    .defaults({ wishlists: [] })
+    .get("wishlists")
+    .find({ id: wishlistId })
+    .cloneDeep()
+    .value();
+
+  const fullBooks = wishlist.books
+    ? wishlist.books.map(b =>
+        db
+          .get("books")
+          .find({ id: b })
+          .cloneDeep()
+          .value()
+      )
+    : [];
+  wishlist.books = fullBooks;
+
+  return wishlist;
+};
+
+server.get("/wishlists/:wishlistId/books", (req, res) => {
+  const wishlist = getBooksForWishlist(
+    parseInt(req.params.wishlistId),
+    router.db
+  );
+  res.status(200).jsonp(wishlist);
+});
+
+server.get("/households/:householdId/wishlistBooks", (req, res) => {
+  const db = router.db;
+
+  const users = db
+    .get("users")
+    .filter({ householdId: parseInt(req.params.householdId, 10) })
+    .cloneDeep()
+    .value();
+
+  const wishlists = users.map(u => getBooksForWishlist(u.wishlistId, db));
+
+  const books = wishlists.map(w => w.books.map(b => b));
+
+  res.status(200).jsonp(flatten(books));
+});
+
+server.get("/books/search", (req, res) => {
+  const db = router.db;
+
+  const books = db.get("books").filter(r =>
+    (req.query.title ? r.title.toLowerCase().includes(req.query.title.toLowerCase()) : true) &&
+    (req.query.author ? r.author.toLowerCase().includes(req.query.author.toLowerCase()) : true)).value();
+
+  res.status(200).send(books);
 });
 
 server.use(router);
